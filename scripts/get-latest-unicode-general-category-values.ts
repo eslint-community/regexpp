8676bbaa389a51db6aa91f8bfc5a2de253975be9
yosuke ota <otameshiyo23@gmail.com>
2023-03-15 15:31:13 +0900

    refactor

--- scripts/get-latest-unicode-general-category-values.ts
+++ scripts/get-latest-unicode-general-category-values.ts
@@ -1,24 +1,11 @@
-import { fetchLines } from "./fetch-lines"
+import { getLatestUnicodePropertyValues } from "./get-latest-unicode-property-values"
 
-const DB_URL =
-    "https://unicode.org/Public/UCD/latest/ucd/PropertyValueAliases.txt"
-const logger = console
 export async function* getLatestUnicodeGeneralCategoryValues(): AsyncIterable<string> {
-    logger.log("Fetching data... (%s)", DB_URL)
-    for await (const line of fetchLines(DB_URL)) {
-        if (!line || line.startsWith("#")) {
-            continue
-        }
-        const [propertyAlias, alias, canonical, ...remaining] = line
-            .split("#")[0] // strip comments
-            .split(";") // split by semicolon
-            .map((x) => x.trim()) // trim
-        if (propertyAlias !== "gc") {
+    for await (const value of getLatestUnicodePropertyValues()) {
+        if (value.propertyAlias !== "gc") {
             continue
         }
 
-        yield alias
-        yield canonical
-        yield* remaining
+        yield* value.aliases
     }
 }
