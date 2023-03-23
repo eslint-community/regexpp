a8bd4e2217483ee23b69c5cf10a8f812f72f6e09
yosuke ota <otameshiyo23@gmail.com>
2023-03-23 09:22:08 +0900

    chore: use node-fetch

--- scripts/fetch-lines.ts
+++ scripts/fetch-lines.ts
@@ -1,66 +1,25 @@
-import https from "https"
-
-export function fetchLines(url: string): AsyncIterable<string> {
-    const linesBuffer: Promise<IteratorResult<string>>[] = []
-
-    let resolve: (val: IteratorResult<string>) => void = () => {
-            // empty function
-        },
-        reject: (reason?: any) => void = resolve
-
-    function pushNext() {
-        linesBuffer.push(
-            new Promise<IteratorResult<string>>((res, rej) => {
-                resolve = res
-                reject = rej
-            }),
-        )
-    }
-
-    function resolveLine(line: string) {
-        resolve({ value: line })
-        pushNext()
-    }
-
-    pushNext()
-
-    const itr: AsyncIterable<string> = {
-        [Symbol.asyncIterator](): AsyncIterator<string> {
-            return {
-                next(): Promise<IteratorResult<string>> {
-                    return linesBuffer.shift()!
-                },
+export async function* fetchLines(url: string): AsyncIterable<string> {
+    const response = await fetch(url)
+    const reader = response.body!.getReader()
+    let buffer = ""
+    const decoder = new TextDecoder()
+    while (true) {
+        const { done, value: chunk } = await reader.read()
+        if (done) {
+            if (chunk) {
+                buffer += decoder.decode(chunk)
             }
-        },
+            yield buffer
+            break
+        }
+        const lines = (buffer + decoder.decode(chunk)).split("\n")
+        if (lines.length === 1) {
+            buffer = lines[0]
+        } else {
+            buffer = lines.pop()!
+            for (const line of lines) {
+                yield line
+            }
+        }
     }
-    https
-        .get(url, (res) => {
-            let buffer = ""
-            res.setEncoding("utf8")
-            res.on("data", (chunk) => {
-                const lines = (buffer + String(chunk)).split("\n")
-                if (lines.length === 1) {
-                    buffer = lines[0]
-                } else {
-                    buffer = lines.pop()!
-                    for (const line of lines) {
-                        resolveLine(line)
-                    }
-                }
-            })
-            res.on("end", () => {
-                if (buffer) {
-                    resolveLine(buffer)
-                }
-                resolve({ done: true, value: undefined })
-            })
-            res.on("error", (e) => {
-                reject(e)
-            })
-        })
-        .on("error", (e) => {
-            reject(e)
-        })
-
-    return itr
 }
