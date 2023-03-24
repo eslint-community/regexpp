7eba7e733942a2a1695b4a7aff3fbe4b0c73ec2d
yosuke ota <otameshiyo23@gmail.com>
2023-03-24 10:27:59 +0900

    chore: use fetch().text()

--- scripts/fetch-lines.ts
+++ scripts/fetch-lines.ts
@@ -1,19 +1,4 @@
 export async function* fetchLines(url: string): AsyncIterable<string> {
     const response = await fetch(url)
-    let buffer = ""
-    const decoder = new TextDecoder()
-    // Already supports AsyncIterable in Node.js v18
-    const body = response.body as unknown as AsyncIterable<Uint8Array>
-    for await (const chunk of body) {
-        const lines = (buffer + decoder.decode(chunk)).split("\n")
-        if (lines.length === 1) {
-            buffer = lines[0]
-        } else {
-            buffer = lines.pop()!
-            for (const line of lines) {
-                yield line
-            }
-        }
-    }
-    yield buffer
+    yield* (await response.text()).split("\n")
 }
